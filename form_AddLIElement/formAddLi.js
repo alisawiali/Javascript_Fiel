a97ed6f3e38fb592503409ce56fe14525b8db8e4
alisawiali <alisawiali70@gmail.com>
2023-12-18 16:45:04 +0100

    Update and vremove all Li list

--- form_AddLIElement/formAddLi.js
+++ form_AddLIElement/formAddLi.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const nameInput = document.getElementById("nameInput");
   const ulElemnt = document.getElementById("student-list");
   const checkboxInput = document.getElementById("checkbox");
+  const LiItems = document.querySelectorAll(".li-Item");
   addStudentButton.addEventListener("click", (event) => {
     event.preventDefault();
     // checkbox wird gechecked
@@ -10,6 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
       alert("Pleacen AGB read");
       // bei retren wird angehalten , wenn den User nicht auf dem Box checked
       return;
+    }
+    // Hier füge den Event-Listener für das Löschen des aktuellen li-Elements hinzu
+    const removeLiElemnt = (liElemnt) => {
+      liElemnt.remove();
+    };
+    //  remove Li list
+    for (const litem of LiItems) {
+      litem.addEventListener("click", () => {
+        removeLiElemnt(litem);
+      });
     }
     const text = nameInput.value;
     if (text) {
@@ -21,12 +32,13 @@ document.addEventListener("DOMContentLoaded", () => {
       liElemnt.appendChild(strongElemnt);
       ulElemnt.appendChild(liElemnt);
 
-      // Die Input leer  weder erstellen
-      nameInput.value = "";
       // Hier füge den Event-Listener für das Löschen des aktuellen li-Elements hinzu
       liElemnt.addEventListener("click", () => {
-        liElemnt.remove();
+        removeLiElemnt(liElemnt);
       });
+      // Die Input leer  weder erstellen
+      nameInput.value = "";
+
       return;
     } else {
       alert("Please enter a valid name!");
