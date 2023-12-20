0121064abd46773418d45c660dc7f4f4e91458ba
alisawiali <alisawiali70@gmail.com>
2023-12-20 13:34:00 +0100

    textDecoration:line-through and mainDiv.remove();

--- Todo-list-Task/todo.js
+++ Todo-list-Task/todo.js
@@ -4,6 +4,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const newTodoTaskElemnt = document.getElementById("newTaskField");
   const todoListUlElement = document.querySelector(".toto-list");
 
+  //  Collback function(maindiv) gegeben , der ruft destory aus dutton
+  const addCollback = (mainDiv) => {
+    const destoryBtn = mainDiv.querySelector(".destory");
+    const checkboxToggle = mainDiv.querySelector(".toggle");
+
+    destoryBtn.addEventListener("click", (evnt) => {
+      if (checkboxToggle.checked) {
+        //   const listItem = mainDiv.closest("li");
+        mainDiv.remove();
+      }
+    });
+    // Event-Listener, um das Styling des strong-Elements zu ändern
+    checkboxToggle.addEventListener("change", () => {
+      const strongElement = mainDiv.querySelector("strong");
+      if (checkboxToggle.classList.contains("compted")) {
+        strongElement.style.textDecoration = "line-through";
+      } else {
+        strongElement.style.textDecoration = "none";
+      }
+    });
+  };
+
   newTodoTaskElemnt.addEventListener("click", (evnt) => {
     evnt.preventDefault();
     const text = newTodoTaskElemnt.value;
@@ -14,8 +36,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const checkbox = document.createElement("input");
       checkbox.type = "checkbox";
-      checkbox.className = "toggel";
-      checkbox.checked;
+      checkbox.className = "toggle";
+      checkbox.checked = true; // Wenn du das Kontrollkästchen standardmäßig aktivieren möchtest
+      checkbox.style.color = "red";
 
       const newdiv = document.createElement("div");
       newdiv.appendChild(newButton);
@@ -24,22 +47,22 @@ document.addEventListener("DOMContentLoaded", () => {
       const newString = document.createElement("strong");
       newString.appendChild(document.createTextNode(text));
 
-      const liElemnt = document.createElement("li");
-      liElemnt.className = "completed";
-      liElemnt.appendChild(newString);
-      liElemnt.appendChild(newdiv);
-
       const newDivElemnt = document.createElement("div");
-      newDivElemnt.classList.add("main");
-      newDivElemnt.appendChild(liElemnt);
+      newDivElemnt.className = "main";
+      newDivElemnt.appendChild(newString);
+      newDivElemnt.appendChild(newdiv);
 
-      todoListUlElement.appendChild(newDivElemnt);
+      const liElemnt = document.createElement("li");
+      liElemnt.appendChild(newDivElemnt);
+
+      todoListUlElement.appendChild(liElemnt);
 
       newTodoTaskElemnt.value = ""; // Leere das Eingabefeld
 
-      newButton.addEventListener("click", () => {
-        liElemnt.remove();
-      });
+      //  CollBack Function der sich beim zahl
+      addCollback(newDivElemnt);
     }
   });
 });
+
+
