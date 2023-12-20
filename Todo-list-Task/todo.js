b3e45e5214466651341c6a2873032895c3e1db78
alisawiali <alisawiali70@gmail.com>
2023-12-20 16:50:48 +0100

    Update count und remove durch btn

--- Todo-list-Task/todo.js
+++ Todo-list-Task/todo.js
@@ -3,6 +3,39 @@ const KEY_ENTER = 13;
 document.addEventListener("DOMContentLoaded", () => {
   const newTodoTaskElemnt = document.getElementById("newTaskField");
   const todoListUlElement = document.querySelector(".toto-list");
+  // footer Block
+  const footerElemnt = document.querySelector(".footer");
+  const todoCountElemnt = document.querySelector(".todo-count strong");
+  const clearBtn = document.querySelector(".clearBtn");
+  //
+  const refercheFooter = () => {
+    if (footerElemnt.children.length === 0) {
+      footerElemnt.style.display = "none";
+    } else {
+      footerElemnt.style.display = "test";
+    }
+    // let count = 0;
+    // for (const ulElemntCount of todoListUlElement.children) {
+    //   if (!ulElemntCount.classList.contains("completed")) {
+    //     count++;
+    //   }
+    //todoCountElemnt.innerText = count;
+    // }
+    // ----------------- Oder ----------- Damit die Zahl angezeigt wird.z.b wie viel Elemnte wurde hingefügt.
+    const countElemnt =
+      todoListUlElement.querySelectorAll("li:not(.completed)").length;
+    todoCountElemnt.innerText = `Item ${countElemnt} `;
+
+    //  (BUTTON): wenn aud checkbox angeklegt dann wird btn angezeigt.
+    const completedCountClaer =
+      todoListUlElement.querySelectorAll("li .completed").length;
+    if (completedCountClaer === 0) {
+      clearBtn.style.display = "none";
+    } else {
+      clearBtn.style.display = "";
+    }
+  };
+  refercheFooter();
 
   //  Collback function(maindiv) gegeben , der ruft destory aus dutton
   const addCollback = (mainDiv) => {
@@ -11,18 +44,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     destoryBtn.addEventListener("click", (evnt) => {
       if (checkboxToggle.checked) {
-        //   const listItem = mainDiv.closest("li");
         mainDiv.remove();
+        refercheFooter();
       }
     });
     // Event-Listener, um das Styling des strong-Elements zu ändern
     checkboxToggle.addEventListener("change", () => {
       const strongElement = mainDiv.querySelector("strong");
-      if (checkboxToggle.classList.contains("compted")) {
+      if (checkboxToggle.checked) {
+        mainDiv.classList.add("completed");
         strongElement.style.textDecoration = "line-through";
       } else {
+        mainDiv.classList.remove("completed");
         strongElement.style.textDecoration = "none";
       }
+      refercheFooter();
     });
   };
 
@@ -36,7 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const checkbox = document.createElement("input");
       checkbox.type = "checkbox";
-      checkbox.className = "toggle";
+      checkbox.classList.add("toggle");
       checkbox.checked = true; // Wenn du das Kontrollkästchen standardmäßig aktivieren möchtest
       checkbox.style.color = "red";
 
@@ -58,10 +94,18 @@ document.addEventListener("DOMContentLoaded", () => {
       todoListUlElement.appendChild(liElemnt);
 
       newTodoTaskElemnt.value = ""; // Leere das Eingabefeld
-
+      refercheFooter();
       //  CollBack Function der sich beim zahl
       addCollback(newDivElemnt);
     }
+  });
+  // mit dem clearBtn wird all gelöschgt, wenn angekrezt wird.
+  clearBtn.addEventListener("click", (evnt) => {
+    const completedClear = todoListUlElement.querySelectorAll("li .completed");
+    for (const todoclearElemnt of completedClear) {
+      todoclearElemnt.remove();
+    }
+    refercheFooter();
   });
 });
 
